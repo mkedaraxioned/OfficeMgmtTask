deaf90049fdaa8b9b82ff985aa6d5610017be92c
kedarmayekar <kedarmayekar077@gmail.com>
2021-06-15 11:15:09 +0530

    Employee view and delete with id added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ prompt.start();
 class OfficeMgmtApp {
 
   init = async () => {
-    console.log(`\n 1.Get all Employee. \n 2.Add an Employee. \n 3.Delete an Employee. \n 4.Do you want to Exit. \n `);
+    console.log(`\n 1.Get all Employee. \n 2.Add an Employee. \n 3.Delete an Employee. \n 4.View an Employee. \n 5.Do you want to Exit. \n `);
 
     console.log('\n Please enter the index no of operation to perform: ');  
   const {input} = await prompt.get(['input']);
@@ -22,9 +22,12 @@ class OfficeMgmtApp {
         this.deleteEntry();
         break;
       case 4:
-        process.exit(0);
+        this.readEntry();
         break;
- 
+      case 5:
+          process.exit(0);
+        break;
+   
       default:
         console.log('Please enter a valid input');
         this.init();
@@ -68,29 +71,33 @@ class OfficeMgmtApp {
 
 
 // JSON READ Single entry
-  readEntry = (id) => {
-  fs.readFile('employee.json','utf8',(err,data) => {
-    if(err) { 
-      console.log(new Error('Somthing went wrong !')); 
-    } else {
-      const recData = JSON.parse(data);
-      let reqData;
-      recData.forEach((post, i) => {
-        if(post.id==id) {
-          reqData = post;
-        }
-
-      });
-
-        for (const key in reqData) {
-          console.log(`${key} : ${reqData[key]}` );
-        }
-        console.log(`\n`);
-        this.init();
+  readEntry = async () => {
+    this.drawAsteriskLine();
+    console.log('Please enter the ID of entry to View: ');
+    let {id} = await prompt.get(['id']);
   
+    fs.readFile('employee.json','utf8',(err,data) => {
+      if(err) { 
+        console.log(new Error('Somthing went wrong !')); 
+      } else {
+        const recData = JSON.parse(data);
+        let reqData;
+        recData.forEach((post, i) => {
+          if(post.id==id) {
+            reqData = post;
+          }
 
-    }
-  });
+        });
+
+          for (const key in reqData) {
+            console.log(`${key} : ${reqData[key]}` );
+          }
+          console.log(`\n`);
+          this.init();
+    
+
+      }
+    });
 
 }
 
@@ -144,10 +151,22 @@ storeToJSON = data => {
         }
         const name =  result.name;
         const email =  result.email;
+        let repeatFlag = false;
 
-        // Add new data to the JSON object
-        recData.push({  "id": newId,  "name": name,  "email": email });
-        this.storeToJSON(recData);
+          recData.forEach((employee, i) => {
+            let addEmpEmail = employee.email;
+            if (addEmpEmail == email) {
+              repeatFlag = true;
+            }
+        });
+        if(!repeatFlag) {
+          // Add new data to the JSON object
+          recData.push({  "id": newId,  "name": name,  "email": email });
+          this.storeToJSON(recData);
+        } else {
+          console.log('Sorry this email is already registered with us');
+          this.init();
+        }
       });
     
   
@@ -158,8 +177,8 @@ storeToJSON = data => {
 // Delete from entries
    deleteEntry = async () => {
      this.drawAsteriskLine();
-  console.log('Please enter the Name of entry to Delete: ');
-  let {name} = await prompt.get(['name']);
+  console.log('Please enter the ID of entry to Delete: ');
+  let {id} = await prompt.get(['id']);
 
   
   fs.readFile('employee.json','utf8',(err, data) => {
@@ -177,8 +196,8 @@ storeToJSON = data => {
         let recData = JSON.parse(data);
         if (result.action.toLowerCase()=='y'||result.action.toLowerCase()=='yes') {
           recData.forEach((employee, i) => {
-            let deleteEmpName =employee.name;
-            if (deleteEmpName.toLowerCase() == name.toLowerCase()) {
+            let deleteEmpId =employee.id;
+            if (deleteEmpId == parseInt(id)) {
                 recData.splice(i, 1);
 
             }
